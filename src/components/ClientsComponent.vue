d85e15f814b0789553ebe3986e1a04bd2c403041
Edemone <edem.agbenyo@gmail.com>
2019-10-17 07:30:34 +0000

    Add images to the services

--- src/components/ClientsComponent.vue
+++ src/components/ClientsComponent.vue
@@ -6,20 +6,20 @@
       <div class="row">
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="" alt="">
+            <img class="img-fluid rounded-circle mb-3" src="../assets/img/gaf.png" alt="">
             <h5>Ghana Armed Forces</h5>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="" alt="">
+            <img class="img-fluid rounded-circle mb-3" src="../assets/img/gaf.png" alt="">
             <h5>Ghana Navy</h5>
             
           </div>
         </div>
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="" alt="">
+            <img class="img-fluid rounded-circle mb-3" src="../assets/img/gaf.png" alt="">
             <h5>Nursing School</h5>
           </div>
         </div>
--- src/components/ServiceComponent.vue
+++ src/components/ServiceComponent.vue
@@ -4,8 +4,7 @@
     <div class="container-fluid p-0">
       <div class="row no-gutters">
 
-        <div class="col-lg-6 order-lg-2 text-white showcase-img"
-          style="background-image: url('img/micro-serv.jpg');"></div>
+        <div class="col-lg-6 order-lg-2 text-white showcase-img service1"></div>
         <div class="col-lg-6 order-lg-1 my-auto showcase-text">
           <h2>Microbiology</h2>
           <p class="lead mb-0">Our microbiology laboratory provides a full range of diagnostic services for the detection and monitoring of bacterial and fungal infections.
@@ -13,15 +12,14 @@
         </div>
       </div>
       <div class="row no-gutters">
-        <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/haem-serv.jpg');"></div>
+        <div class="col-lg-6 text-white showcase-img service2"></div>
         <div class="col-lg-6 my-auto showcase-text">
           <h2>Haematology</h2>
           <p class="lead mb-0"> Our haematology laboratory provides consistent quality results in all aspects of routine and specialised haematology, flow cytometry and bone marrow assessment.</p>
         </div>
       </div>
       <div class="row no-gutters">
-        <div class="col-lg-6 order-lg-2 text-white showcase-img"
-          style="background: url('img/chemistry-serv.jpg') no-repeat center 50px/500px ;"></div>
+        <div class="col-lg-6 order-lg-2 text-white showcase-img service3"></div>
         <div class="col-lg-6 order-lg-1 my-auto showcase-text">
           <h2>Chemistry</h2>
           <p class="lead mb-0"> Our haematology laboratory provides consistent quality results in all aspects of routine and specialised haematology, flow cytometry and bone marrow assessment.</p>
@@ -41,5 +39,26 @@ export default {
 </script>
 
 <style>
-
+.service1{
+  height: 400px;
+  background-image: url("../assets/img/micro-serv.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100%;
+}
+.service2{
+  height: 400px;
+  background-image: url("../assets/img/haem-serv.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100%;
+}
+.service3{
+  height: 400px;
+  background-color: #F3EEF4;
+  background-image: url("../assets/img/chemistry-serv.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 50%;
+}
 </style>
